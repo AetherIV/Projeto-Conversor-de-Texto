b17d6feb486932c8167c2fc83a03c7e7d82ff2fd
AetherIV <102974135+AetherIV@users.noreply.github.com>
2024-05-17 16:31:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -48,11 +48,11 @@ function convert() {
     }
     if (toggleCNPJ) {
         modifiedText = modifiedText.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/g, "$1.$2.$3/$4-$5");
-    }
-    if (toggleCPF) {
+    }else if (toggleCPF) {
         modifiedText = modifiedText.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, "$1.$2.$3-$4");
     }
 
+    
     document.getElementById(`outputText`).innerText = modifiedText; //Template literal to help with placeholders
 
 }
